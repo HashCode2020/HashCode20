d8ee4e8cadbb1a25d441e3f68613218f088c6e6d
David Vila <dvfernandez@esei.uvigo.es>
2020-02-20 20:11:03 +0100

    Ids listos

--- Ronda 1/Ej1.py
+++ Ronda 1/Ej1.py
@@ -7,8 +7,12 @@ class Library:
         self.books = books
 
     def __str__(self) -> str:
-        return "Libros {0} tiempo registro {1} tiempo envio {2} libros {3}".format(self.numBooks, self.signupTime,
-                                                                                   self.shipping, len(self.books))
+        return "Biblio: {0} Libros {1} tiempo registro {2} tiempo envio {3} libros {4}".format(self.id, self.numBooks,
+                                                                                               self.signupTime,
+                                                                                               self.shipping,
+                                                                                               len(self.books))
+
+
 class Book:
     def __init__(self, id, puntos):
         self.id = id
@@ -40,10 +44,10 @@ def puntosPotencialesConsigueLibreria(libreria, diasDisponibles):
     libroSeleccionado = 0
 
     # mientras queden dias para registrar libros, se cogen los libros y se registran aumentando la puntuacion
-    while(diasDisponiblesDespuesRegistro > 0):
+    while (diasDisponiblesDespuesRegistro > 0):
         librosDisponiblesDia = libreria.shipping
 
-        while(librosDisponiblesDia > 0):
+        while (librosDisponiblesDia > 0):
             puntuacion += libreria.books[libroSeleccionado]
             libroSeleccionado += 1
             librosDisponiblesDia -= 1
@@ -64,8 +68,9 @@ numDays = int(array[0].split()[2])
 score = array[1].split()
 array = array[2:]
 libraries = []
+idCutreParaBibliotecas = 0
 
-for i in range(0, numLibraries+1, +2):
+for i in range(0, numLibraries + 1, +2):
     numBooks = array[i].split()[0]
     signupTime = array[i].split()[1]
     shipping = array[i].split()[2]
@@ -73,9 +78,10 @@ for i in range(0, numLibraries+1, +2):
     lista_books = []
 
     for book in books:
-        lista_books.append(Book(int(book), score[int(book)-1]))
+        lista_books.append(Book(int(book), score[int(book) - 1]))
 
-    libraries.append(Library(i, numBooks, signupTime, shipping, lista_books))
+    libraries.append(Library(idCutreParaBibliotecas, numBooks, signupTime, shipping, lista_books))
+    idCutreParaBibliotecas = idCutreParaBibliotecas + 1
 
 for i in libraries:
     print("{0}\r\n".format(i))
