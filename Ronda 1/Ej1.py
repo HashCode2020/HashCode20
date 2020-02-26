7de3cc4ddd5f96d8c7399345f5c97c9c60cc67e4
fralvarezz <fralvarez@esei.uvigo.es>
2020-02-26 18:54:14 +0100

    Mejoras

--- Ronda 1/Ej1.py
+++ Ronda 1/Ej1.py
@@ -24,22 +24,28 @@ class Book:
     def __gt__(self, other):
         return self.puntos > other.puntos
 
+    def __eq__(self, other):
+        return self.id == other.id
+
+    def __hash__(self):
+        return self.id
+
     def __str__(self) -> str:
         return "Libro {0}: {1}".format(self.id, self.puntos)
+
 
 
 # librerias: Array de librerias
 # diasMax: Dias restantes para escanear libros
 # return: devulve la libreria con mayor puntos/dia
-def mejorBiblioteca(bibliotecas, diasMax):
-    puntosMax = 0;
+def mejorBiblioteca(bibliotecas, diasMax, poolLibros):
+    puntosMax = 0
     puntos=0
     for biblio in bibliotecas:
-        puntos = puntosPotencialesConsigueLibreria(biblio, diasMax)
+        puntos = puntosPotencialesConsigueLibreria(biblio, diasMax, poolLibros)
         if puntosMax <= puntos:
             puntosMax = puntos
-            mejor_biblio = biblio;
-
+            mejor_biblio = biblio
     return mejor_biblio
 
 
@@ -63,27 +69,44 @@ def printToFile(bibliotecas):
 
 
 # Funcion que define los puntos que puede conseguir una libreria en funcion de los dias que quedan
-def puntosPotencialesConsigueLibreria(libreria, diasDisponibles):
+def puntosPotencialesConsigueLibreria(libreria, diasDisponibles, poolLibros):
+    poolInterno = set(poolLibros)
     diasDisponiblesDespuesRegistro = diasDisponibles - libreria.signupTime
     puntuacion = 0
     libroSeleccionado = 0
 
+    quedanLibros = False
+    #comprobar que queden libros de la biblioteca en el pool
+    for libro in libreria.books:
+        if libro in poolInterno:
+            quedanLibros = True
+    #quedanLibros = libreria.books in poolInterno
+
     # mientras queden dias para registrar libros, se cogen los libros y se registran aumentando la puntuacion
-    while (diasDisponiblesDespuesRegistro > 0):
+    while (diasDisponiblesDespuesRegistro > 0 and quedanLibros):
         librosDisponiblesDia = libreria.shipping
 
-        while (librosDisponiblesDia > 0):
-            puntuacion += int(libreria.books[libroSeleccionado].puntos)
-            if libroSeleccionado < libreria.numBooks - 1:
-                libroSeleccionado += 1
-            librosDisponiblesDia -= 1
+        while (librosDisponiblesDia > 0 and quedanLibros):
+
+            if(libreria.books[libroSeleccionado] in poolInterno):
+                puntuacion += int(libreria.books[libroSeleccionado].puntos)
+                poolInterno.remove(libreria.books[libroSeleccionado])
+                if libroSeleccionado < libreria.numBooks - 1:
+                    libroSeleccionado += 1
+                librosDisponiblesDia -= 1
+
+            quedanLibros = False
+            for libro in libreria.books:
+                if libro in poolInterno:
+                    quedanLibros = True
 
         diasDisponiblesDespuesRegistro -= 1
+
 
     return puntuacion
 
 
-def escogeLibros(libreria, diasDisponibles):
+def escogeLibros(libreria, diasDisponibles,poolLibros):
     librosSeleccionados = []
     diasDisponiblesDespuesRegistro = diasDisponibles - libreria.signupTime
     libroSeleccionado = 0
@@ -91,8 +114,9 @@ def escogeLibros(libreria, diasDisponibles):
     while (diasDisponiblesDespuesRegistro > 0):
         librosDisponiblesDia = libreria.shipping
 
-        while (librosDisponiblesDia > 0 and libroSeleccionado < libreria.numBooks - 1):
+        while (librosDisponiblesDia > 0 and libroSeleccionado < libreria.numBooks - 1 and libreria.books[libroSeleccionado] in poolLibros):
             librosSeleccionados.append(libreria.books[libroSeleccionado])
+            poolLibros.discard(libreria.books[libroSeleccionado])
             libroSeleccionado += 1
 
             librosDisponiblesDia -= 1
@@ -102,7 +126,7 @@ def escogeLibros(libreria, diasDisponibles):
 
 
 # Leer archivo
-f = open('a_example.txt')
+f = open('d_tough_choices.txt')
 read = f.read()
 array = read.split("\n")
 
@@ -113,21 +137,27 @@ score = array[1].split()
 array = array[2:]
 libraries = []
 idCutreParaBibliotecas = 0
+poolLibros = set()
 
 for i in range(0, numLibraries + 1, +2):
     numBooks = array[i].split()[0]
     signupTime = array[i].split()[1]
     shipping = array[i].split()[2]
     books = array[(i + 1)].split()
+    #print(books)
     lista_books = []
 
     for book in books:
-        lista_books.append(Book(int(book), score[int(book) - 1]))
+        libro = Book(int(book), score[int(book) - 1])
+        lista_books.append(libro)
+        poolLibros.add(libro)
 
+    #poolLibros.update(lista_books)
     lista_books.sort(reverse=True)
     libraries.append(Library(idCutreParaBibliotecas, numBooks, signupTime, shipping, lista_books))
     idCutreParaBibliotecas = idCutreParaBibliotecas + 1
 
+#print(poolLibros)
 
 ###MAIN
 
@@ -135,8 +165,9 @@ libreriasOrdenadas = []
 resultado = []
 
 while numDays > 0 and len(libraries) > 0:
-    libreriaEscogida = mejorBiblioteca(libraries, numDays)
-    librosEscogidos = escogeLibros(libreriaEscogida, numDays)
+    libreriaEscogida = mejorBiblioteca(libraries, numDays,poolLibros)
+    librosEscogidos = escogeLibros(libreriaEscogida, numDays,poolLibros)
+    print(poolLibros)
 
     libreriasResultado = Library(libreriaEscogida.id, len(librosEscogidos), libreriaEscogida.signupTime,
                                  libreriaEscogida.shipping, librosEscogidos)
@@ -144,7 +175,7 @@ while numDays > 0 and len(libraries) > 0:
     resultado.append(libreriasResultado)
     # eliminamos lo que escogimos
 
-    print(libraries)
+    #print(libraries)
 
     libraries.remove(libreriaEscogida)
     numDays -= libreriaEscogida.signupTime
